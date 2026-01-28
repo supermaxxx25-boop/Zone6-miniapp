ea22245b5a487d2164ed3ab443e2f57f9136df2e
supermaxxx25-boop <supermaxxx25@icloud.com>
2026-01-28 17:48:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+Telegram.WebApp.ready()
+Telegram.WebApp.expand()
 
+Telegram.WebApp.MainButton.setText("VALIDER COMMANDE")
+Telegram.WebApp.MainButton.show()
+
+Telegram.WebApp.MainButton.onClick(() => {
+  Telegram.WebApp.sendData(
+    JSON.stringify({
+      test: "OK"
+    })
+  )
+})
