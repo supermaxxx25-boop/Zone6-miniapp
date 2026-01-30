02640552b3f99ebbe7f72d4db5652569805a3ccb
supermaxxx25-boop <supermaxxx25@icloud.com>
2026-01-30 19:27:39 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,11 @@
 from telegram import Update
-from telegram.ext import (
-    ApplicationBuilder,
-    CommandHandler,
-    ContextTypes
-)
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "👋 Bot OK ! Étape 1 validée ✅"
-    )
+    await update.message.reply_text("👋 Bot OK ! Étape 1 validée ✅")
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
